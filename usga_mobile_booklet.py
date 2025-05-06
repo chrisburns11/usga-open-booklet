43fec6d42ec3a0e8c444951db43e57a5219deac4
chrisburns11 <ChristopherBurns11@Gmail.com>
2025-05-05 22:07:46 -0400

    Update usga_mobile_booklet.py

--- usga_mobile_booklet.py
+++ usga_mobile_booklet.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # --- Page Config ---
-st.set_page_config(page_title="USGA U.S. Open Player Memo", layout="centered", page_icon="🏌️")
+st.set_page_config(page_title="USGA U.S. Open Player Memo", layout="wide", page_icon="🏌️")
 
 # --- Hide Streamlit UI ---
 st.markdown("""
