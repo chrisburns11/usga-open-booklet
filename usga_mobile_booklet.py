f8315bd0e0581a811970bee66be62fd20fc449e6
chrisburns11 <ChristopherBurns11@Gmail.com>
2025-05-09 21:40:06 -0400

    Update usga_mobile_booklet.py

--- usga_mobile_booklet.py
+++ usga_mobile_booklet.py
@@ -3,9 +3,6 @@ import streamlit as st
 # --- Page Config ---
 st.set_page_config(page_title="USGA U.S. Open Player Memo", layout="wide", page_icon="https://idss-proxy.imgix.net/https%3A%2F%2Ffiles.idss.com%2FC32%2F0efcbc6c-d7cc-4aa2-9ee5-ca7e854a3fe3.png?auto=compress%2Cformat&fit=max&h=1080&q=80&w=1920&s=25f63e35e4c282d2d2a004f9827045c7")
 
-# Force wide layout
-st.set_page_config(layout="wide")
-
 # --- Hide Streamlit UI Elements ---
 st.markdown("""
     <style>
