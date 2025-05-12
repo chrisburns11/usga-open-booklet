f8ac8a018cb1627397f64472e9e52762fc606d01
chrisburns11 <ChristopherBurns11@Gmail.com>
2025-05-12 14:26:16 -0400

    Update usga_mobile_booklet.py

--- usga_mobile_booklet.py
+++ usga_mobile_booklet.py
@@ -3,6 +3,26 @@ import streamlit as st
 # --- Page Config ---
 st.set_page_config(page_title="USGA U.S. Open Player Memo", layout="wide", page_icon="https://idss-proxy.imgix.net/https%3A%2F%2Ffiles.idss.com%2FC32%2F0efcbc6c-d7cc-4aa2-9ee5-ca7e854a3fe3.png?auto=compress%2Cformat&fit=max&h=1080&q=80&w=1920&s=25f63e35e4c282d2d2a004f9827045c7")
 
+# Inject CSS for styling toggle
+st.markdown("""
+    <style>
+    /* Change toggle label color */
+    label[data-testid="stToggleLabel"] {
+        color: #555555;  /* light grey/dark text for visibility */
+        font-weight: bold;
+    }
+
+    /* Adjust toggle knob and background */
+    div[data-testid="stToggle"] > div {
+        background-color: #ccc !important;  /* inactive background */
+    }
+
+    div[data-testid="stToggle"] > div > div {
+        background-color: #666 !important;  /* toggle knob */
+    }
+    </style>
+""", unsafe_allow_html=True)
+
 col1, = st.columns([1])
 
 with col1:
