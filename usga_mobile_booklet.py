f0225cf40c29f0627b650e813d4c51ea56fb5020
chrisburns11 <ChristopherBurns11@Gmail.com>
2025-05-12 14:18:31 -0400

    Update usga_mobile_booklet.py

--- usga_mobile_booklet.py
+++ usga_mobile_booklet.py
@@ -3,12 +3,9 @@ import streamlit as st
 # --- Page Config ---
 st.set_page_config(page_title="USGA U.S. Open Player Memo", layout="wide", page_icon="https://idss-proxy.imgix.net/https%3A%2F%2Ffiles.idss.com%2FC32%2F0efcbc6c-d7cc-4aa2-9ee5-ca7e854a3fe3.png?auto=compress%2Cformat&fit=max&h=1080&q=80&w=1920&s=25f63e35e4c282d2d2a004f9827045c7")
 
-col1, col2 = st.columns([1, 2])
+col1 = st.columns([1])
 
 with col1:
-    st.markdown("☀️", unsafe_allow_html=True)
-
-with col2:
     dark_mode = st.toggle("🌙 / ☀️", value=False, label_visibility="visible")
 
 # Apply dark or light mode styles
