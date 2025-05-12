814b1c4dc3267202504f049a7c32cd66503ca782
chrisburns11 <ChristopherBurns11@Gmail.com>
2025-05-12 15:03:27 -0400

    Update usga_mobile_booklet.py

--- usga_mobile_booklet.py
+++ usga_mobile_booklet.py
@@ -34,8 +34,8 @@ def apply_theme(is_dark):
             """
             <style>
             html, body, .stApp {
-                background-color: #ffffff;
-                color: #ffffff;
+                background-color: #f2f2f2;
+                color: #f2f2f2;
             }
             .stMarkdown, .stText, .stDataFrame, .stButton > button, .stSlider, .stSelectbox {
                 color: #f5f5f5 !important;
